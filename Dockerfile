88c907003782b24a34681d714b592c764d83e5d6
Corean Namja Al Fathir <reina@hoshimachi.local>
2026-02-19 17:53:41 +0700

    Changing structure to a workspace instead of 2 separate package

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,12 @@
 FROM rust:1.93-alpine
+
+WORKDIR /server
+
+COPY ./cce2e_server/ .
+
+RUN cargo check &&\
+    cargo build -r
+
+EXPOSE 7007
+
+CMD [ "/" ]
--- cc_server/src/main.rs
+++ cc_server/src/main.rs
@@ -1,15 +1,11 @@
 use std::net;
 
-use clap::Parser;
-
 fn main() {
-    cce2e_rs::args::AppArgs::parse();
-
-    let listener: net::TcpListener = cce2e_rs::start_listening();
+    let listener: net::TcpListener = cc_server::start_listening();
 
     for connection in listener.incoming() {
         match connection {
-            Ok(mut stream_info) => cce2e_rs::handle_client(&mut stream_info),
+            Ok(mut stream_info) => cc_server::handle_client(&mut stream_info),
             Err(err) => {
                 println!("Failed to connect, {}", err);
                 return;
